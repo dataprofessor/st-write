ab782fec7380bad42df3e1bb52870e3421081b2a
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-03-17 20:14:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.subheader('This is using st.subheader')
 st.write('This is using st.write')
 st.markdown('This is using st.markdown')
 
-x = 'This is from a variable and displayed in Streamlit using the built-in magic command'
+x = 'This text is from a variable and displayed in Streamlit via the built-in magic by simply calling the variable.'
 x
