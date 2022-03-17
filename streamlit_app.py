89210895da2c1ed0fc46ea0b7a58165e1af2eb8d
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-03-17 20:02:31 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.header('This is using st.header')
+st.subheader('This is using st.subheader')
+st.write('This is using st.write')
+st.markdown('This is using st.markdown')
+
+x = 'This is from a variable and displayed in Streamlit using the built-in magic command'
+x
