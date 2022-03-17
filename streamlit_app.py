c89ca7c84b895e32682d5ba10dc32a9836e3d4c1
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-03-17 20:17:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.header('This is using st.header')
 st.subheader('This is using st.subheader')
 st.write('This is using st.write')
-st.markdown('This is using st.markdown')
+st.markdown("This is using st.markdown, which can leverage markdown's functionality, e.g. *bold*, **italic**, ***bold and italic***, `highlights`, etc.")
 
 x = 'This text is from a variable and displayed in Streamlit via the built-in magic by simply calling the variable.'
 x
